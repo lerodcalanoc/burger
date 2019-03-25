03a542a9f56990fbbe115e9ad0a7953bab7b6d28
lerodcalanoc <lerodcalanoc@gmail.com>
2019-03-25 08:18:11 -0700

    modified

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: process.env.BURGERS_HOST,
     port: 3306,
     user: process.env.BURGERS_USER,
     password: process.env.BURGERS_PASSWORD,
     database: 'burgers_db'
-})
+  });
+};
+
 
 connection.connect(function(err) {
   if (err) {
