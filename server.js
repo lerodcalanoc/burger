e6e06cb087240a6edc6f06600cdbba190fe5a863
lerodcalanoc <lerodcalanoc@gmail.com>
2019-03-25 08:06:09 -0700

    port updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.static(process.cwd() + "/"));
